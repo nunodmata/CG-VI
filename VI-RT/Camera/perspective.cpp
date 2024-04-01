59c6e8866601706f8acc887409b4776442f0b1a0
Filipagit <76223623+Filipagit@users.noreply.github.com>
2024-04-01 19:22:51 +0100

    camera done

--- VI-RT/Camera/perspective.cpp
+++ VI-RT/Camera/perspective.cpp
@@ -6,7 +6,31 @@
 //
 
 #include "perspective.hpp"
+#include <iostream>
 
 bool Perspective::GenerateRay(const int x, const int y, Ray *r, const float *cam_jitter) {
-    return false;
+     //Camera Space 
+     float xc, yc;
+    if (cam_jitter == NULL) {
+        xc = 2.f * ((float)x + .5f) / W - 1.f;
+        yc = 2.f * ((float)(H - y - 1) + .5f) / H - 1.f;
+    }
+    else {
+        xc = 2.f * ((float)x + cam_jitter[0]) / W - 1.f;
+        yc = 2.f * ((float)(H - y - 1) + cam_jitter[1]) / H - 1.f;
+    }
+
+    Vector dirc=Vector(xc,yc,1.0f);
+
+    //Camera Setup
+    Vector* dir_cw=new Vector();
+    //ray*dir
+    dir_cw->X = c2w[0][0] * dirc.X + c2w[0][1] * dirc.Y + c2w[0][2] * dirc.Z;
+    dir_cw->Y = c2w[1][0] * dirc.X + c2w[1][1] * dirc.Y + c2w[1][2] * dirc.Z;
+    dir_cw->Z = c2w[2][0] * dirc.X + c2w[2][1] * dirc.Y + c2w[2][2] * dirc.Z;
+    
+    // set origin and direction of the ray
+    r->o=Eye;
+    r->dir=*dir_cw;
+    return true;
 }
--- VI-RT/Camera/perspective.hpp
+++ VI-RT/Camera/perspective.hpp
@@ -21,6 +21,22 @@ class Perspective: public Camera {
 public:
     Perspective (const Point Eye, const Point At, const Vector Up, const int W, const int H, const float fovW, const float fovH): Eye(Eye), At(At), Up(Up), W(W), H(H), fovW(fovW), fovH(fovH)  {
         // compute camera 2 world transform
+         //Camera Setup
+         Vector f = Eye.vec2point(At);
+         f.normalize();
+         Vector r=f.cross(Up);
+         r.normalize();
+         Vector u = Up;
+        //Create Matrix c2w
+        c2w[0][0] = r.X;
+        c2w[0][1] = r.Y;
+        c2w[0][2] = r.Z;
+        c2w[1][0] = u.X;
+        c2w[1][1] = u.Y;
+        c2w[1][2] = u.Z;
+        c2w[2][0] = f.X;
+        c2w[2][1] = f.Y;
+        c2w[2][2] = f.Z;
     }
     bool GenerateRay(const int x, const int y, Ray *r, const float *cam_jitter=NULL);
     void getResolution (int *_W, int *_H) {*_W=W; *_H=H;}

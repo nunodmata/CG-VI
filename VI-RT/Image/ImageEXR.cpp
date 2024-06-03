ce0146b3255ceb8ace847caf2dd670f2924369f9
nunodmata <nunomata619@gmail.com>
2024-06-03 11:08:19 +0100

    small update

--- VI-RT/Image/ImageEXR.cpp
+++ VI-RT/Image/ImageEXR.cpp
@@ -1,5 +1,6 @@
 #include "ImageEXR.hpp"
 
+
 void ImageEXR::ToneMap(cv::Mat &hdr_image) {
     for (int i = 0; i < H; ++i) {
         for (int j = 0; j < W; ++j) {
@@ -25,7 +26,6 @@ void ImageEXR::ToneMap(cv::Mat &hdr_image) {
     }
 }
 
-
 ImageEXR::ImageEXR(Image &img){
     W = img.getW();
     H = img.getH();
@@ -33,6 +33,24 @@ ImageEXR::ImageEXR(Image &img){
     imagePlane = img.getImagePlane();
 }
 
+// if you want to save the image without tone mapping
+
+// bool ImageEXR::Save(std::string filename) {
+//     cv::Mat hdr_image(H, W, CV_32FC3);
+
+//     for (int i = 0; i < H; i++)
+//         for (int j = 0; j < W; j++) {
+//             RGB &rgb = imagePlane[i*W+j];
+//             hdr_image.at<cv::Vec3f>(i, j) = cv::Vec3f(rgb.B, rgb.G, rgb.R);
+//         }
+
+//     std::vector<int> exr_params;
+//     exr_params.push_back(cv::IMWRITE_EXR_TYPE);
+//     exr_params.push_back(cv::IMWRITE_EXR_TYPE_FLOAT); //specify the datatype of a pixel. Here it is set as float
+
+//     cv::imwrite(filename, hdr_image, exr_params);
+//     return true;
+// }
 
 bool ImageEXR::Save(std::string filename) {
     cv::Mat hdr_image(H, W, CV_32FC3);
